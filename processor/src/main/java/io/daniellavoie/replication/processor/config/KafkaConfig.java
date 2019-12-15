0a39f3a025f9f210e3d508c2011c76434cbdbbd3
Daniel Lavoie <dlavoie@live.ca>
2019-12-15 18:11:48 -0500

    Add support for Basic Auth on Connect Rest API
    Closes #1

--- processor/src/main/java/io/daniellavoie/replication/processor/config/KafkaConfig.java
+++ processor/src/main/java/io/daniellavoie/replication/processor/config/KafkaConfig.java
@@ -4,6 +4,8 @@ import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.daniellavoie.replication.processor.connect.ConnectConfiguration;
@@ -17,6 +19,15 @@ public class KafkaConfig {
 
 	@Bean
 	public WebClient webClient(ConnectConfiguration connectConfiguration) {
+		WebClient.Builder builder = WebClient.builder().baseUrl(connectConfiguration.getUrl());
+
+		if (!connectConfiguration.getBasicUser().trim().equals("")
+				&& !connectConfiguration.getBasicPassword().trim().equals("")) {
+			builder.defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + Base64Utils.encodeToString(
+					(connectConfiguration.getBasicUser().trim() + ":" + connectConfiguration.getBasicPassword().trim())
+							.getBytes()));
+		}
+
 		return WebClient.create(connectConfiguration.getUrl());
 	}
 }
--- processor/src/main/java/io/daniellavoie/replication/processor/connect/ConnectConfiguration.java
+++ processor/src/main/java/io/daniellavoie/replication/processor/connect/ConnectConfiguration.java
@@ -1,5 +1,6 @@
 package io.daniellavoie.replication.processor.connect;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,11 +9,33 @@ import org.springframework.context.annotation.Configuration;
 public class ConnectConfiguration {
 	private String url = "http://localhost:8083";
 
+	@Value("${connect.basic.user}")
+	private String basicUser = "";
+
+	@Value("${connect.basic.password}")
+	private String basicPassword = "";
+
 	public String getUrl() {
 		return url;
 	}
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	public String getBasicUser() {
+		return basicUser;
+	}
+
+	public void setBasicUser(String basicUser) {
+		this.basicUser = basicUser;
+	}
+
+	public String getBasicPassword() {
+		return basicPassword;
+	}
+
+	public void setBasicPassword(String basicPassword) {
+		this.basicPassword = basicPassword;
 	}
 }
